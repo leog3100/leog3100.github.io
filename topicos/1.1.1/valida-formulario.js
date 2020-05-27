0fc604dfc521218f1d93c5b60bff3f956681baf4
leog3100 <leonardo.gomes@unigranrio.br>
2020-05-27 20:37:50 -0300

    Js Novo

--- topicos/1.1.1/valida-formulario.js
+++ topicos/1.1.1/valida-formulario.js
@@ -1,4 +1,4 @@
-function validacao(recaptchaCallback) {
+function validacao() {
     if(document.form.nome.value=="") {
         alert("Por favor, preencha o Nome.");
         document.form.nome.focus();
@@ -11,17 +11,17 @@ function validacao(recaptchaCallback) {
         return false;
     }
 
-    if (!document.getElementById('recaptcha-id').value=="") {
+    if (document.querySelector("#recaptcha-anchor").value==="") {
         alert("Você não preencheu o Captcha de verificação.");
         return false;
     }else {
-        recaptchaCallback();
+        document.querySelector("body > div.container > form > input:nth-child(9)").toggleAttribute=('disabled, true');
     }
 
 }
+//document.querySelector("#recaptcha-anchor > div.recaptcha-checkbox-checkmark")
+//function recaptchaCallback()
+//{
+   //document.getElementsByName("Submit") = jQuery('#submitBtn').prop('disabled', true);
 
-function recaptchaCallback()
-{
-   document.getElementsByName("Submit") = jQuery('#submitBtn').prop('disabled', true);
-
-}
+//}
